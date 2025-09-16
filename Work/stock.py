485bb44535cbe99aa348c036b29c87c1832255e3
栗功哲-23371506 <23371506@buaa.edu.cn>
2025-09-16 11:18:46 +0800

    Unit 5 completed

--- Work/stock.py
+++ Work/stock.py
@@ -1,4 +1,5 @@
 class Stock:
+    __slots__ = ('name', '_shares', 'price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -7,6 +8,17 @@ class Stock:
     def __repr__(self):
         return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
 
+    @ property
+    def shares(self):
+        return self._shares
+
+    @ shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected an integer')
+        self._shares = value
+
+    @ property
     def cost(self):
         return self.shares * self.price
 
